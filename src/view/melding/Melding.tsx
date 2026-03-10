d8a728b48449ed10513b27a61194d54fc826cb82
Sigurd Grøneng <sigurdgroneng@gmail.com>
2026-03-10 09:58:57 +0100

    Dont render dash-headings or code-blocks (#644)
    * Dont render dash-headings or code-blocks
    
    * Finish comments
    
    * Fix types
    
    * Remove dup css

--- src/view/melding/Melding.tsx
+++ src/view/melding/Melding.tsx
@@ -1,13 +1,13 @@
 import { ExternalLinkIcon, PersonIcon } from '@navikt/aksel-icons';
 import remarkBreaks from 'remark-breaks';
 import { BodyShort, Chat } from '@navikt/ds-react';
-import React from 'react';
+import React, { ClassAttributes, FunctionComponent, HTMLAttributes } from 'react';
 
 import { ViktigMelding } from '../../felleskomponenter/etiketer/Etikett';
 import { formaterDateAndTime } from '../../utils/Date';
 import { MeldingsData } from '../../utils/Typer';
 import { useUserInfoContext } from '../BrukerProvider';
-import Markdown from 'react-markdown';
+import Markdown, { ExtraProps } from 'react-markdown';
 import { linkifyToMarkdown } from './linkify';
 
 function accessibleText(erBruker: boolean, erMeldingFraBruker: boolean) {
@@ -57,15 +57,15 @@ export function Melding(props: Props) {
                             <Markdown
                                 remarkPlugins={[remarkBreaks]}
                                 components={{
-                                    a: ({ node, ...props }) => (
-                                        <span className="inline-flex items-center">
-                                            <a {...props} target="_blank" rel="noopener noreferrer" />
-                                            <ExternalLinkIcon
-                                                className="ml-1 inline-block"
-                                                aria-label="Lenke åpnes i ny fane"
-                                            />
-                                        </span>
-                                    ),
+                                    /* Don't render headings (h1) created by adding equal signs on the next line as headings, render as normal p + hr instead */
+                                    h1: renderMarkdownH1Tag,
+                                    /* Don't render headings (h2) created by adding dash on the next line as headings, render as normal p + hr instead */
+                                    h2: renderMarkdownH2Tag,
+                                    /* Don't render the pre-tag at all, just render the children */
+                                    pre: renderMarkdownPreTag,
+                                    /* Don't render the code-tag, render it as a normal p instead */
+                                    code: renderMarkdownCodeTag,
+                                    a: renderMarkdownATag,
                                 }}
                                 disallowedElements={['script']}
                             >
@@ -78,3 +78,58 @@ export function Melding(props: Props) {
         </div>
     );
 }
+
+const renderMarkdownATag: FunctionComponent<
+    ClassAttributes<HTMLAnchorElement> & HTMLAttributes<HTMLAnchorElement> & ExtraProps
+> = ({ node, ...props }) => (
+    <span className="inline-flex items-center">
+        <a {...props} target="_blank" rel="noopener noreferrer" />
+        <ExternalLinkIcon className="ml-1 inline-block" aria-label="Lenke åpnes i ny fane" />
+    </span>
+);
+
+const isMarkdownHeadingCreatedUsingDashOrEqualSignOnNextLine = (
+    node: { children: { type: string; position?: { start: { column: number } } }[] } | undefined,
+) => {
+    const firstChild = node?.children[0];
+    if (firstChild?.type !== 'text') return false;
+    // If position start with 1 it cant be an in-line markdown heading
+    return firstChild?.position?.start?.column === 1;
+};
+
+const renderMarkdownH1Tag: FunctionComponent<
+    ClassAttributes<HTMLHeadingElement> & HTMLAttributes<HTMLHeadingElement> & ExtraProps
+> = ({ node, children }) => {
+    if (isMarkdownHeadingCreatedUsingDashOrEqualSignOnNextLine(node)) {
+        return (
+            <>
+                <p>{children}</p>
+                <hr />
+            </>
+        );
+    }
+    return <h1>children</h1>;
+};
+const renderMarkdownH2Tag: FunctionComponent<
+    ClassAttributes<HTMLHeadingElement> & HTMLAttributes<HTMLHeadingElement> & ExtraProps
+> = ({ node, children }) => {
+    if (isMarkdownHeadingCreatedUsingDashOrEqualSignOnNextLine(node)) {
+        return (
+            <>
+                <p>{children}</p>
+                <hr />
+            </>
+        );
+    }
+    return <h2>children</h2>;
+};
+const renderMarkdownPreTag: FunctionComponent<
+    ClassAttributes<HTMLPreElement> & HTMLAttributes<HTMLPreElement> & ExtraProps
+> = ({ node, children, ...props }) => {
+    return children;
+};
+const renderMarkdownCodeTag: FunctionComponent<
+    ClassAttributes<HTMLElement> & HTMLAttributes<HTMLElement> & ExtraProps
+> = ({ node, children, ...props }) => {
+    return <p>{children}</p>;
+};
